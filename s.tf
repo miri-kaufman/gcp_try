58217909934c41a8e6055132ab222af0c2dcc39b
miri_kaufman <mirikaufman4121@gmail.com>
2024-06-30 14:20:09 +0300

    add_resource

--- s.tf
+++ s.tf
@@ -7,5 +7,9 @@ resource "aws_subnet" "eks_subnet" {
   tags = {
     Name                                    = "${local.prefix}-eks-subnet-1"
     "kubernetes.io/cluster/${local.prefix}" = "shared"
+    yor_name                                = "eks_subnet"
+    yor_trace                               = "615a0786-446a-48af-9e4b-b11f80695f7b"
+    module_yor_name                         = "eks_subnet"
+    module_yor_trace                        = "36c3c5e5-ecb6-4abb-9f17-383af195e5f4"
   }
 }
