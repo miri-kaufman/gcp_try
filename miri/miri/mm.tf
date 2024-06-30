a3090c92d215f916b3f279cd65f82ce11d8873fa
miri_kaufman <mirikaufman4121@gmail.com>
2024-06-30 15:18:52 +0300

    one__

--- miri/miri/mm.tf
+++ miri/miri/mm.tf
@@ -2,7 +2,7 @@ resource "google_storage_bucket" "static-site" {
   name          = "image-store.com"
   location      = "EU"
   force_destroy = true
-
+  
   uniform_bucket_level_access = true
 
   website {
