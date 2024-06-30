8d160f2e5ff9ee3504e5e6af7b09d61a9af80b3a
miri_kaufman <mirikaufman4121@gmail.com>
2024-06-30 15:13:04 +0300

    add

--- r.tf
+++ r.tf
@@ -17,16 +17,16 @@ resource "google_storage_bucket" "static-site" {
     max_age_seconds = 3600
   }
   labels = {
-    git_commit                  = "58217909934c41a8e6055132ab222af0c2dcc39b"
+    git_commit                  = "0e6215dd5bd666118b6bcd5c4b19eda57b3b83a9"
     git_file                    = "r_tf"
-    git_last_modified_at        = "2024-06-30-11-20-09"
+    git_last_modified_at        = "2024-06-30-11-31-57"
     git_last_modified_by        = "mirikaufman4121"
     git_modifiers               = "mirikaufman4121"
     git_org                     = "miri-kaufman"
     git_repo                    = "gcp_try"
     yor_name                    = "static-site"
     yor_trace                   = "55d70032-2057-4036-a1f5-5fac1afc7697"
-    module_git_commit           = "58217909934c41a8e6055132ab222af0c2dcc39b"
+    module_git_commit           = "0e6215dd5bd666118b6bcd5c4b19eda57b3b83a9"
     module_git_file             = "r.tf"
     module_git_last_modified_at = "2024-06-30 11:19:50"
     module_git_last_modified_by = "mirikaufman4121@gmail.com"
